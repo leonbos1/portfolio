238b0975da82ecd39fc644b75a7e614d0964e949
leonbos1 <ktmleon@live.nl>
2023-01-15 15:18:49 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       <div id="home" class="home">
         <p class="introduction">
           Hello, my name is <span class="name">Leon Bos</span><br />
-          I am a Software Engineering Student
+          I am a Software Engineering Student test
         </p>
         <div class="nav">
           <button @click="scrollToNextPage('about')">About Me</button>
